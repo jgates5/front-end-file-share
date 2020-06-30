d2426efcaa59ae31cad30294e06aab694c0af55a
jeffwgates <jeff.gates0507@gmail.com>
2020-06-30 14:24:12 -0600

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -8,24 +8,25 @@ export default class App extends Component {
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleDownload = this.handleDownload.bind(this);
-    this.handlesubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleGetData = this.handleGetData.bind(this);
   }
 
-handleChange(event) {
-  this.setState({ file: event.target.files[0] })
-}
+  handleChange(event) {
+    this.setState({ file: event.target.files[0] })
+  }
 
-handleDownload() {
-  const url = window.URL.createObjectURL(this.state.file)
-  const link = document.createElement("a")
-  link.download = this.state.filenName
-  link.href = url
-  document.body.appendChild(link)
-  link.click()
-  link.remove()
-  window.URL.revokeObjectURL(url)
+  handleDownload() {
+    const url = window.URL.createObjectURL(this.state.file)
+    const link = document.createElement("a")
+    link.download = this.state.filenName
+    link.href = url
+    document.body.appendChild(link)
+    link.click()
+    link.remove()
+    window.URL.revokeObjectURL(url)
 
-}
+  }
 
 handleSubmit() {
   const form = new FormData()
@@ -43,11 +44,23 @@ handleSubmit() {
 }
 
 
+handleGetData() {
+  fetch("http://127.0.0.1:5000/file/get/1", { method: "GET" })
+  .then(response => console.log(response))
+  .then(data => {
+    const file = new File([data], "Test.jpg", { type: "image/jpeg" })
+    this.setState({ file: file })
+  })
+  .catch(error => console.log("error"))
+}
+
   render() {
     return (
       <div className='app'>
         <input onChange={this.handleChange} type="file"/>
         <button onClick={this.handleSubmit}>Submit</button>
+        <hr/>
+        <button onClick={this.handleGetData}>Get File</button>
         <button onClick={this.handleDownload}>Download</button>
       </div>
     );
