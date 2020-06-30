6c91b4d25bc3bc0b6c8a54443c200753a2d81b7e
jeffwgates <jeff.gates0507@gmail.com>
2020-06-30 09:58:13 -0600

    file upload and upload

---
+++ src/components/app.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      file:{}
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDownload = this.handleDownload.bind(this);
+  }
+
+handleChange(event) {
+  this.setState({ file: event.target.files[0] })
+}
+
+handleDownload() {
+  const url = window.URL.createObjectURL(this.state.file)
+  const link = document.createElement("a")
+  link.download = this.state.filenName
+  link.href = url
+  document.body.appendChild(link)
+  link.click()
+  link.remove()
+  window.URL.revokeObjectURL(url)
+
+}
+
+  render() {
+    return (
+      <div className='app'>
+        <input onChange={this.handleChange} type="file"/>
+        <button onClick={this.handleDownload}>Download</button>
+      </div>
+    );
+  }
+}
